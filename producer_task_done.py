0ba0b90cd139c1466d582d0254b1b841d5aca113
Alex Kiura <kiuraalex@gmail.com>
2021-01-11 10:42:40 +0300

    Scaffold producer consumer using task done

---
+++ producer_task_done.py
@@ -0,0 +1,19 @@
+import asyncio
+import random
+
+
+async def produce(queue, n):
+    pass
+
+
+async def consume(queue, n):
+    pass
+
+
+async def run(n):
+    pass
+
+
+loop = asyncio.get_event_loop()
+loop.run_until_complete(run(10))
+loop.close()
