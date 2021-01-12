f46106d6043bff9c09795a0c9a996cebb73fdbcb
Alex Kiura <kiuraalex@gmail.com>
2021-01-12 12:22:43 +0300

    Producer consumer using task done

--- producer_task_done.py
+++ producer_task_done.py
@@ -3,15 +3,36 @@ import random
 
 
 async def produce(queue, n):
-    pass
+    for x in range(n):
+        print(f"producing {x}/{n}")
+        # simulate io operation using sleep
+        await asyncio.sleep(random.random())
+        item = str(x)
+        # put the item in the queue
+        await queue.put(item)
 
 
-async def consume(queue, n):
-    pass
+async def consume(queue):
+    while True:
+        # wait for an item from the producer
+        item = await queue.get()
+
+        # process the item
+        print(f"consuming item {item}....")
+        # simulate io operation
+        await asyncio.sleep(random.random())
 
 
 async def run(n):
-    pass
+    queue = asyncio.Queue()
+    # schedule the consumer
+    consumer = asyncio.ensure_future(consume(queue))
+    # run the producer and wait for completion
+    await produce(queue, n)
+    # wait until the consumer has processed all items
+    await queue.join()
+
+    consumer.cancel()
 
 
 loop = asyncio.get_event_loop()
